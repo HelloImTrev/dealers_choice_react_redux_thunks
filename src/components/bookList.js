7e5242d811708a21dcf91b7f5e16e3652357e452
Trevor Latimer <trevorplatimer@gmail.com>
2022-03-17 17:02:31 -0500

    got delete function working

--- src/components/bookList.js
+++ src/components/bookList.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { connect } from "react-redux";
-
+import { deleteBook } from "../redux/store";
 
 const BookList = (props) => {
+
+  const books = useSelector(state => state.books);
+  const dispatch = useDispatch();
+
   return (
     <div className="book-list">
       <table>
@@ -11,12 +16,12 @@ const BookList = (props) => {
             <th>Title</th>
             <th>Author</th>
           </tr>
-          {props.books.map((book) => {
+          {books.map((book) => {
             return (
               <tr key={book.id}>
                 <td>{book.title}</td>
                 <td>{book.author.name}</td>
-                <td><button>Delete</button></td>
+                <td><button onClick={() => dispatch(deleteBook(book))}>Delete</button></td>
               </tr>
             );
           })}
@@ -26,11 +31,21 @@ const BookList = (props) => {
   );
 };
 
-const mapState = (state) => {
-  return {
-    books: state.books,
-    author: state.authors,
-  };
-}
+// const mapState = (state) => {
+//   return {
+//     books: state.books,
+//     author: state.authors,
+//   };
+// }
 
-export default connect(mapState, {})(BookList);
+// const mapDispatch = (dispatch) => {
+//   return{
+//     delete: (book) => {
+//       dispatch(deleteBook(book));
+//     }
+//   } 
+// }
+
+//export default connect(mapState, mapDispatch)(BookList);
+
+export default BookList;
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,28 +2,39 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunks from "redux-thunk";
 import axios from "axios";
 
+//REDUCERS
 const bookReducer = (books = [], action) => {
-  switch(action.type) {
+  switch (action.type) {
     case "GET_BOOKS":
       books = action.books;
     case "DELETE_BOOK":
-      books = books.filter(book => book.id !== action.book)
+      books = books.filter((book) => {
+        return book.id !== action.book});
     default:
-      return books
+      return books;
   }
 };
 
+//THUNKS
 export const getBooks = () => {
   return async (dispatch) => {
-    const res = await axios.get('/api/books');
-    dispatch({ type: "GET_BOOKS", books: res.data})
-  }
-}
+    const res = await axios.get("/api/books");
+    dispatch({ type: "GET_BOOKS", books: res.data });
+  };
+};
 
+export const deleteBook = (book) => {
+  return async (dispatch) => {
+    await axios.delete(`/api/books/${book.id}`);
+    dispatch({ type: "DELETE_BOOK" , book: book.id});
+  };
+};
+
+//ROOT REDUCER
 const reducer = combineReducers({
   books: bookReducer,
   //TODO: Add author reducer
-})
+});
 
 const store = createStore(reducer, applyMiddleware(thunks));
 
